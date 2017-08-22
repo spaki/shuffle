70b9f50a6fbbe98747a04e5e42f62d7d64d1c221
Eduardo Spaki <eduardo.spaki@farfetch.com>
2017-08-22 09:56:27 +0100

    refactoring

--- Shuffle/Shuffle/ExtensionMethods.cs
+++ Shuffle/Shuffle/ExtensionMethods.cs
@@ -1,15 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace Shuffle
 {
     public static class ExtensionMethods
     {
-        public static IOrderedEnumerable<T> Shuffle<T>(this IEnumerable<T> items)
-        {
-            return items.OrderBy(e => Guid.NewGuid());
-        }
+        public static IOrderedEnumerable<T> Shuffle<T>(this IEnumerable<T> items) => items.OrderBy(e => Guid.NewGuid());
     }
 }
