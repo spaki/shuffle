2757b1a2f3da3cbb273ef9d8fa5965986fa89e72
Eduardo Spaki <eduardo.spaki@farfetch.com>
2017-08-22 09:39:25 +0100

    first commit

---
+++ Shuffle/Shuffle/ExtensionMethods.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Shuffle
+{
+    public static class ExtensionMethods
+    {
+        public static IOrderedEnumerable<T> Shuffle<T>(this IEnumerable<T> items)
+        {
+            return items.OrderBy(e => Guid.NewGuid());
+        }
+    }
+}
---
+++ Shuffle/Shuffle/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Shuffle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stringValues = new string[] { "Batman", "Darkseid", "Flash", "Harley Quinn", "Joker", "Superman", "Wonder Woman" };
+            var intValues = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            Console.WriteLine(string.Join(", ", stringValues.Shuffle()));
+            Console.WriteLine(string.Join(", ", stringValues.Shuffle()));
+
+            Console.WriteLine(string.Join(", ", intValues.Shuffle()));
+            Console.WriteLine(string.Join(", ", intValues.Shuffle()));
+
+            Console.ReadLine();
+        }
+    }
+}
